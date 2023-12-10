786ea6a1724db6e90b80358777f78aaa7ca88d54
NuhaAlfarhani <muhammad.difa.21@student.se.ittelkom-sby.ac.id>
2023-12-10 16:56:20 +0700

    Fixing vertically scrollable component infinite height

--- app/src/main/java/com/example/petapp/Page/Penitipan/Penitipan.kt
+++ app/src/main/java/com/example/petapp/Page/Penitipan/Penitipan.kt
@@ -2,19 +2,31 @@ package com.example.petapp.Page.Penitipan
 
 import android.content.Context
 import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -29,6 +41,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -68,7 +81,7 @@ fun Penitipan(navController: NavController, context: Context = LocalContext.curr
         .build()
         .create(PenitipanService::class.java)
     val call = retrofit.getData()
-    call.enqueue(object : Callback<List<PenitipanRespon>>{
+    call.enqueue(object : Callback<List<PenitipanRespon>> {
         override fun onResponse(
             call: Call<List<PenitipanRespon>>,
             response: Response<List<PenitipanRespon>>
@@ -137,6 +150,7 @@ fun Penitipan(navController: NavController, context: Context = LocalContext.curr
                         fontFamily = FontFamily(Font(R.font.poppins_regular))
                     )
                 },
+                shape = RoundedCornerShape(30.dp),
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(bottom = 6.dp),
@@ -157,42 +171,51 @@ fun Penitipan(navController: NavController, context: Context = LocalContext.curr
                 )
             )
 
-//            LazyColumn{
-//                listPenitipan.forEach{penitipan ->
-//                    item {
-//                        Row(
-//                            verticalAlignment = Alignment.CenterVertically,
-//                            modifier = Modifier
-//                                .fillMaxWidth()
-//                                .clickable { navController.navigate("") }
-//                        ) {
-//                            Box{
-//                                Image(
-//                                    painter = ,
-//                                    contentDescription = null,
-//                                    alignment = Alignment.Center,
-//                                    modifier = Modifier
-//                                        .height(100.dp)
-//                                        .width(100.dp)
-//                                        .padding(end = 12.dp)
-//                                        .clickable { navController.navigate("") }
-//                                )
-//                            }
-//                            Text(
-//                                text = penitipan.nama_penitipan,
-//                                fontFamily = FontFamily(Font(R.font.poppins_medium))
-//                            )
-//                        }
-//
-//                        Divider(
-//                            modifier = Modifier
-//                                .fillMaxWidth()
-//                                .height(1.dp)
-//                                .background(Color.Gray)
-//                        )
-//                    }
-//                }
-//            }
+            LazyColumn(
+                modifier = Modifier.weight(1f)
+            ){
+                listPenitipan.forEach{penitipan ->
+                    item {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                                .clickable { navController.navigate("homepage") }
+                        ) {
+                            Box(
+                                modifier = Modifier
+                                    .size(100.dp)
+                                    .clip(MaterialTheme.shapes.medium)
+                            ){
+                                Image(
+                                    painter = artikel1,
+                                    contentDescription = null,
+                                    alignment = Alignment.Center,
+                                    modifier = Modifier
+                                        .height(100.dp)
+                                        .width(100.dp)
+                                        .padding(end = 12.dp)
+                                        .clickable { navController.navigate("homepage") }
+                                )
+
+                            }
+                            Spacer(modifier = Modifier.width(16.dp))
+                            Text(
+                                text = penitipan.nama_penitipan,
+                                fontFamily = FontFamily(Font(R.font.poppins_medium))
+                            )
+                        }
+
+                        Divider(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(1.dp)
+                                .background(Color.Gray)
+                        )
+                    }
+                }
+            }
         }
 
     }
