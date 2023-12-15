79639a33ba2b3c0d5fea8de83e5e3b8012da2404
NuhaAlfarhani <muhammad.difa.21@student.se.ittelkom-sby.ac.id>
2023-12-15 11:50:47 +0700

    add penitipan detil page

--- app/src/main/java/com/example/petapp/Page/Penitipan/PenitipanDetil.kt
+++ app/src/main/java/com/example/petapp/Page/Penitipan/PenitipanDetil.kt
@@ -3,30 +3,23 @@ package com.example.petapp.Page.Penitipan
 import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -41,7 +34,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -64,31 +56,32 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Penitipan(navController: NavController, context: Context = LocalContext.current){
+fun PenitipanDetil(navController: NavController, context: Context = LocalContext.current){
     val baseColor = Color(0xFF00676C)
     val artikel1 = painterResource(id = R.drawable.artikel1)
+    val toko = painterResource(id = R.drawable.toko)
     var search by remember {
         mutableStateOf(TextFieldValue(""))
     }
-    val listPenitipan = remember {
+    val listProduk = remember {
         mutableStateListOf<PenitipanRespon>()
     }
     var baseUrl = "http://10.0.2.2:1337/api/"
-//    var baseUrl = "http://10.217.17.11:1337/api/"
+//    var baseUrl =  "http://10.217.17.11:1337/api/"
     val retrofit = Retrofit.Builder()
         .baseUrl(baseUrl)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
         .create(PenitipanService::class.java)
     val call = retrofit.getData()
-    call.enqueue(object : Callback<List<PenitipanRespon>> {
+    call.enqueue(object : Callback<List<PenitipanRespon>>{
         override fun onResponse(
             call: Call<List<PenitipanRespon>>,
             response: Response<List<PenitipanRespon>>
         ) {
             if(response.isSuccessful){
-                listPenitipan.clear()
-                response.body()?.forEach{penitipanRespon -> listPenitipan.add(penitipanRespon) }
+                listProduk.clear()
+                response.body()?.forEach{penitipanRespon -> listProduk.add(penitipanRespon) }
             } else{
                 val message = response.errorBody()?.string()?: "Unknown error"
                 print("Error: $message")
@@ -105,9 +98,7 @@ fun Penitipan(navController: NavController, context: Context = LocalContext.curr
     Scaffold(
         topBar = {
             TopAppBar(title = {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
+                Row {
                     IconButton(onClick = { navController.navigate("homepage") }) {
                         Icon(
                             Icons.Default.ArrowBack,
@@ -119,11 +110,7 @@ fun Penitipan(navController: NavController, context: Context = LocalContext.curr
                         text = "Penitipan Hewan",
                         fontWeight = FontWeight.Bold,
                         fontSize = 24.sp,
-                        fontFamily = FontFamily(
-                            Font(
-                                R.font.poppins_semibold
-                            )
-                        )
+                        fontFamily = FontFamily(Font(R.font.poppins_semibold))
                     )
                 }
             },
@@ -133,7 +120,7 @@ fun Penitipan(navController: NavController, context: Context = LocalContext.curr
                 )
             )
         }
-    ) {innerPadding ->
+    ) { innerPadding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -142,13 +129,12 @@ fun Penitipan(navController: NavController, context: Context = LocalContext.curr
                 .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            OutlinedTextField(value = search, onValueChange = { newText ->
-                search = newText
-            },
-                label = {
-                    Text(text = "Search",
-                        fontFamily = FontFamily(Font(R.font.poppins_regular))
-                    )
+            OutlinedTextField(
+                value = search,
+                onValueChange = { newText -> search = newText },
+                label = { Text(
+                    text = "Search",
+                    fontFamily = FontFamily(Font(R.font.poppins_regular)))
                 },
                 shape = RoundedCornerShape(30.dp),
                 modifier = Modifier
@@ -156,7 +142,7 @@ fun Penitipan(navController: NavController, context: Context = LocalContext.curr
                     .padding(bottom = 6.dp),
                 trailingIcon = {
                     IconButton(onClick = {
-                        // Handle the search action
+                    // Handle Search Action
                     }) {
                         Icon(Icons.Default.Search,
                             contentDescription = "Search",
@@ -171,57 +157,77 @@ fun Penitipan(navController: NavController, context: Context = LocalContext.curr
                 )
             )
 
-            LazyColumn(
-                modifier = Modifier.weight(1f)
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 18.dp)
             ){
-                listPenitipan.forEach{penitipan ->
-                    item {
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    Box {
+                        Image(
+                            painter = toko,
+                            contentDescription = null,
+                            alignment = Alignment.Center,
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .padding(16.dp)
-                                .clickable { navController.navigate("homepage") }
-                        ) {
-                            Box(
-                                modifier = Modifier
-                                    .size(100.dp)
-                                    .clip(MaterialTheme.shapes.medium)
-                            ){
-                                Image(
-                                    painter = artikel1,
-                                    contentDescription = null,
-                                    alignment = Alignment.Center,
-                                    modifier = Modifier
-                                        .height(100.dp)
-                                        .width(100.dp)
-                                        .padding(end = 12.dp)
-                                        .clickable { navController.navigate("homepage") }
-                                )
-                            }
-                            Spacer(modifier = Modifier.width(16.dp))
-                            Text(
-                                text = penitipan.nama_penitipan,
-                                fontFamily = FontFamily(Font(R.font.poppins_medium))
-                            )
-                        }
+                                .height(150.dp)
 
-                        Divider(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(1.dp)
-                                .background(Color.Gray)
                         )
                     }
                 }
             }
-        }
 
+//            LazyColumn(
+//                modifier = Modifier.weight(1f)
+//            ){
+//                listProduk.forEach{produk ->
+//                    item {
+//                        Row(
+//                            verticalAlignment = Alignment.CenterVertically,
+//                            modifier = Modifier
+//                                .fillMaxWidth()
+//                                .padding(16.dp)
+//                        ) {
+//                            Box(
+//                                modifier = Modifier
+//                                    .size(100.dp)
+//                                    .clip(MaterialTheme.shapes.medium)
+//                            ) {
+//                                Image(painter = artikel1,
+//                                    contentDescription = null,
+//                                    alignment = Alignment.Center,
+//                                    modifier = Modifier
+//                                        .height(100.dp)
+//                                        .width(100.dp)
+//                                        .padding(end = 12.dp)
+//                                )
+//                            }
+//                            Spacer(modifier = Modifier.width(16.dp))
+//                            Text(
+//                                text = produk.nama_penitipan,
+//                                fontFamily = FontFamily(Font(R.font.poppins_medium))
+//                            )
+//                        }
+//
+//                        Divider(
+//                            modifier = Modifier
+//                                .fillMaxWidth()
+//                                .height(1.dp)
+//                                .background(Color.Gray)
+//                        )
+//                    }
+//                }
+//            }
+        }
     }
 }
 
 @Preview
 @Composable
-fun PenitipanPreview(){
-    Penitipan(navController = NavController(context = LocalContext.current))
+fun PenitipanDetilPreview(){
+    PenitipanDetil(navController = NavController(context = LocalContext.current))
 }
